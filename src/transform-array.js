ad49884d52fc578d6f0df7a988a44fc9c9274135
katya-petrova <walki-disi@ya.ru>
2021-02-26 14:57:42 +0300

    feat: add function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+  newArr.push(arr[i]);
+  }
+  if (newArr[0] === '--double-prev' || newArr[0] === '--discard-prev') { newArr.splice(0, 1);
+  return newArr;
+  }
+  if (newArr[newArr.length -1] === '--double-next' || newArr[newArr.length -1] === '--discard-next') {
+  newArr.splice(newArr.length - 1, 1);
+  return newArr;
+  }
+  doubleNext(newArr);
+  doublePrev(newArr);
+  discardNext(newArr);
+  discardPrev(newArr);
+  return newArr;
+  };
+  
+  function doubleNext(newArr) {
+  task = newArr.indexOf('--double-next');
+  newArr.splice(task, 1, newArr[task + 1]);
+  return newArr;
+  }
+  
+  function doublePrev(newArr) {
+  task2 = newArr.indexOf('--double-prev');
+  newArr.splice(task2, 1, newArr[task2 - 1]);
+  return newArr;
+  }
+  
+  function discardNext(newArr) {
+  task3 = newArr.indexOf('--discard-next');
+  newArr.splice(task3, 2);
+  return newArr;
+  }
+  
+  function discardPrev(newArr) {
+  task4 = newArr.indexOf('--discard-prev');
+  newArr.splice(task4 - 1, 2);
+  return newArr.filter(item => typeof item !== 'string');
+  };

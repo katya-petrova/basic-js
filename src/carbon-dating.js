70a5d25f8ea63940867477998a14859144b197cd
katya-petrova <walki-disi@ya.ru>
2021-03-04 16:49:21 +0300

    fix: correct carbon-dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,8 +5,11 @@ const HALF_LIFE_PERIOD= 5730;
 const k = 0.693 / HALF_LIFE_PERIOD;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof +sampleActivity !== 'string' || isNaN(typeof (sampleActivity)) || Number(sampleActivity) < 0 || Number(sampleActivity) > 15) {
+  let nanCheck = parseFloat(sampleActivity);
+
+  if (typeof sampleActivity !== 'string' || isNaN(nanCheck) || Number(sampleActivity) <= 0 || Number(sampleActivity) > 15) {
     return false;
   }
+
   return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
 };
